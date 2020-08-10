bef662ddb8fd42a3ff07b8c427aa45ff7e301606
dan9l10 <fddfdfdf@fdfdf.fdfd>
2020-08-10 00:24:32 +0000

    Error with mysql (Script not working)

--- autoconfigure_web.sh
+++ autoconfigure_web.sh
@@ -5,6 +5,6 @@ DB=mysql
 VER_PHP="7.4"
 BACKEND_SERV="php$VER_PHP"
 
-apt update && apt install -y $BACKEND_SERV-cgi $BACKEND_SERV $BACKEND_SERV-fpm $BACKEND_SERV-mysql $WEB_SERVER $DB-server && 
-systemctl start mysql && mysql_secure_installation
+apt update && apt install -y $WEB_SERVER && apt install -y $DB-server && mysql_secure_instalalation
+apt install -y $BACKEND_SERV-cgi $BACKEND_SERV $BACKEND_SERV-fpm $BACKEND_SERV-mysql && 
 cp ./nginx.conf /etc/nginx/ && systemctl reload nginx
